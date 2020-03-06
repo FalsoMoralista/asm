7f98729c1c1bd5aee114545d28135328d3de4f17
root <elvis@uefs>
2020-03-06 12:14:51 -0300

    lcd init

--- ARM/game_engine.asm
+++ ARM/game_engine.asm
@@ -1,5 +1,5 @@
 .data
-.equ fopen, 4 
+.equ fopen, 5
 .equ ioctl, 54
 
 lcd_driver: .asciz "/dev/lcd_1602"
@@ -7,15 +7,21 @@ lcd_driver: .asciz "/dev/lcd_1602"
 
 // lcd api
 .equ clear, 1
-
+.text
+.global main
+main:
+	b init_display
 init_display:
-	ldr r0, =lcd_driver 
-	mov r1, #0 
+	ldr r0, =lcd_driver
+	mov r1, #0
 	mov r2, #00700
-	ldr r7, =fopen 
+	ldr r7, =fopen
 	swi 0 // call for file opening
 
 	ldr r7, =ioctl 	
+
 	mov r5, r0 // file descriptor for lcd driver
 	ldr r1, =clear
 	swi 0 // call for clear display
+end:
+
